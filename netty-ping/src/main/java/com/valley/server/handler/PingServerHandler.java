1d0e942531df1822485abdcb6465dcb6de379546
ValleyChen333 <1900766899@qq.com>
2017-09-14 23:01:19 +0800

    响应telnet请求

---
+++ netty-ping/src/main/java/com/valley/server/handler/PingServerHandler.java
@@ -0,0 +1,55 @@
+package com.valley.server.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ----------Dragon be here!----------/
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑
+ * 　　　　┃　　　┃代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━
+ *
+ * @author valley chen
+ * @version 1.0.0
+ * @date 2017/9/14
+ *
+ * Handles a server-side channel.
+ */
+public class PingServerHandler extends ChannelInboundHandlerAdapter{
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buffer = ctx.alloc().buffer(4);
+        buffer.writeBytes("pong\n".getBytes());
+       ctx.writeAndFlush(buffer);
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
